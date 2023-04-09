778dd0cd2abd9b3fbe83f0593e27abe8abccf64b
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-09 19:10:25 +0300

    Make responsive for medium devices

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import RouteSwitch from './RouteSwitch';
+import './styles/media-queries.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
