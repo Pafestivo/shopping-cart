2e4da89cdfe2bdab99d70b2e96649c81ee698a18
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-09 16:09:51 +0300

    Shop now button also reroute

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,18 @@
 import './styles/home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="home">
       <div className="left">
         <h1>New items added to the shop, check them out!</h1>
-        <p className='btn'>Shop now</p>
+        <Link to='/shop'>
+          <p className='btn'>Shop now</p>
+        </Link>
       </div>
 
       <div className="right">
-        <img src={require('./images/dog-house.jpg')} alt="" />
+        <img src={require('./images/dog-house.jpg')} alt="" />        
       </div>
 
     </div>
